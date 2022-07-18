d6c9de962b4904d209c10fd27274f918625fe506
usamamalik32 <usama0834@gmail.com>
2022-07-18 14:56:36 +0500

    cart

--- lib/Detail.dart
+++ lib/Detail.dart
@@ -1,13 +1,79 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:pharmacy/cart.dart';
 import 'package:pharmacy/constraint.dart';
 
 import 'models/product.dart';
 
-class MedDetail extends StatelessWidget {
+class MedDetail extends StatefulWidget {
   final Product product;
-  const MedDetail({Key? key, required this.product}) : super(key: key);
+  MedDetail({Key? key, required this.product}) : super(key: key);
+
+  @override
+  State<MedDetail> createState() => _MedDetailState();
+}
+
+class _MedDetailState extends State<MedDetail> {
+  Future addToCart(int quantity) async {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    var currentUser = _auth.currentUser;
+    int totalprice=widget.product.pPrice*quantity;
+
+    CollectionReference _collectionRef =
+        FirebaseFirestore.instance.collection("cart-items");
+
+    return _collectionRef
+        .doc(currentUser!.email)
+        .collection("items")
+        .doc(widget.product.pID)
+        .set({
+      "name": widget.product.pName,
+      "price": widget.product.pPrice,
+      "images": widget.product.pimageUrl,
+      "Quantity": quantity,
+      "totalprice" : totalprice,
+    }).then((value) => print("Added to cart"));
+  }
+
+  checkcart() {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    var currentUser = _auth.currentUser;
+    bool _sameItemCheck = false;
+
+    final firestoreInstance = FirebaseFirestore.instance;
+    firestoreInstance.collection("cart-items").get().then((querySnapshot) {
+      querySnapshot.docs.forEach((result) {
+        firestoreInstance
+            .collection("cart-items")
+            .doc(currentUser!.email)
+            .collection("items")
+            .get()
+            .then((querySnapshot) {
+          if (querySnapshot.docs
+              .any((element) => element.id == widget.product.pID)) {
+            _sameItemCheck = true;
+
+            Fluttertoast.showToast(
+                msg: "product is already added",
+                toastLength: Toast.LENGTH_LONG,
+                gravity: ToastGravity.BOTTOM,
+                timeInSecForIosWeb: 1,
+                backgroundColor: Colors.white,
+                textColor: primarycolor,
+                fontSize: 18.0);
+          } else {
+            _sameItemCheck = false;
+            addToCart(quantity);
+          }
+        });
+      });
+    });
+  }
+
+  int quantity = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -22,15 +88,18 @@ class MedDetail extends StatelessWidget {
           },
           icon: const Icon(
             Icons.arrow_back,
-            color: Colors.black,
+            color: Colors.white,
           ),
         ),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => Cart()));
+            },
             icon: const Icon(
               Icons.add_shopping_cart,
-              color: Colors.black,
+              color: Colors.white,
             ),
           ),
         ],
@@ -41,7 +110,19 @@ class MedDetail extends StatelessWidget {
             height: MediaQuery.of(context).size.height * .35,
             padding: const EdgeInsets.only(bottom: 30),
             width: double.infinity,
-            child: Image.network(product.pimageUrl),
+            child: Image.network(widget.product.pimageUrl),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: const EdgeInsets.only(top: 10),
+              width: 50,
+              height: 5,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(50),
+              ),
+            ),
           ),
           Expanded(
             child: Stack(
@@ -60,8 +141,8 @@ class MedDetail extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          product.pCategory,
-                          style: TextStyle(
+                          widget.product.pCategory,
+                          style: const TextStyle(
                             fontSize: 15,
                             color: Colors.grey,
                           ),
@@ -70,15 +151,15 @@ class MedDetail extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Text(
-                              product.pName,
-                              style: TextStyle(
+                              widget.product.pName,
+                              style: const TextStyle(
                                 fontSize: 22,
                                 fontWeight: FontWeight.w600,
                               ),
                             ),
                             Text(
-                              product.pPrice.toString(),
-                              style: TextStyle(
+                              widget.product.pPrice.toString(),
+                              style:const TextStyle(
                                 fontSize: 22,
                                 fontWeight: FontWeight.w600,
                               ),
@@ -87,59 +168,56 @@ class MedDetail extends StatelessWidget {
                         ),
                         const SizedBox(height: 15),
                         Text(
-                          product.pDescription,
-                          style: TextStyle(
+                          widget.product.pDescription,
+                          style: const TextStyle(
                             fontSize: 15,
                             color: Colors.grey,
                           ),
                         ),
-                        const SizedBox(height: 15),
-                        // Text(
-                        //   'Similar This',
-                        //   style: TextStyle(
-                        //     fontSize: 16,
-                        //     fontWeight: FontWeight.w600,
-                        //   ),
-                        // ),
-                        const SizedBox(height: 10),
-                        // SizedBox(
-                        //   height: 110,
-                        //   child: ListView.builder(
-                        //     scrollDirection: Axis.horizontal,
-                        //     itemCount: smProducts.length,
-                        //     itemBuilder: (context, index) => Container(
-                        //       margin: const EdgeInsets.only(right: 6),
-                        //       width: 110,
-                        //       height: 110,
-                        //       decoration: BoxDecoration(
-                        //         color: primarycolor,
-                        //         borderRadius: BorderRadius.circular(20),
-                        //       ),
-                        //       // child: Center(
-                        //       //   child: Image(
-                        //       //     height: 70,
-                        //       //     image: AssetImage([index].image),
-                        //       //   ),
-                        //       // ),
-                        //     ),
-                        //   ),
-                        // ),
-                        const SizedBox(height: 20),
+                       
                       ],
                     ),
                   ),
                 ),
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Container(
-                    margin: const EdgeInsets.only(top: 10),
-                    width: 50,
-                    height: 5,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  ),
+                
+              ],
+            ),
+            
+          ),
+        
+         
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+         
+              children: [
+             const Padding(
+                padding:  EdgeInsets.only(right: 150.0),
+                child:   Text("Quantity",  style:  TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w600,
+                                    ),),
+              ),
+             
+                quantity != 0
+                    ? IconButton(
+                        icon: const Icon(Icons.remove),
+                        onPressed: () {
+                          setState(() {
+                            quantity--;
+                          });
+                        },
+                      )
+                    : Container(),
+                Text(quantity.toString()),
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    setState(() {
+                      quantity++;
+                    });
+                  },
                 ),
               ],
             ),
@@ -164,32 +242,36 @@ class MedDetail extends StatelessWidget {
               child: Icon(
                 Icons.heart_broken,
                 size: 30,
-                color: Colors.grey,
+                color: primarycolor,
               ),
             ),
             SizedBox(width: 20),
+            
             Expanded(
               child: InkWell(
                 onTap: () {
+                  checkcart();
+
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => Cart()));
                 },
                 child: Container(
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
                     color: primarycolor,
-                    borderRadius: BorderRadius.circular(15),
+                    borderRadius: BorderRadius.circular(50),
                   ),
                   child: Text(
-                            '+ Add to Cart',
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white,
-                            ),
-                          ),
+                    '+ Add to Cart',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-           
+            ),
           ],
         ),
       ),
