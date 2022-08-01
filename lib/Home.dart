56f952a9a89cd21adc7a92a6e8547ba8e43ff119
usamamalik32 <usama0834@gmail.com>
2022-08-01 15:50:57 +0500

    search option

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,12 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:pharmacy/cart.dart';
 import 'package:pharmacy/login.dart';
+import 'package:pharmacy/categorydetail.dart';
 import 'package:pharmacy/shop.dart';
 
-import 'package:provider/provider.dart';
+
 
 import 'Detail.dart';
 
@@ -37,7 +39,7 @@ class _HomepageState extends State<Homepage> {
   }
 
   fetchproduct() async {
-    QuerySnapshot p = await firestoreInstance.collection("products").get();
+    QuerySnapshot p = await firestoreInstance.collection("products").where("ispopular", isEqualTo: true).get();
     setState(() {
       for (var document in p.docs) {
         _products.add(Product(
@@ -84,16 +86,10 @@ class _HomepageState extends State<Homepage> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(
-                        'Asad',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 25.0),
-                      ),
+                   
                       SizedBox(height: 10.0),
                       Text(
-                        'Asad@gmail.com',
+                         FirebaseAuth.instance.currentUser!.email.toString(),
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             color: Colors.white,
@@ -107,22 +103,26 @@ class _HomepageState extends State<Homepage> {
 
             //Here you place your menu items
             ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home Page', style: TextStyle(fontSize: 18)),
+              leading: const Icon(Icons.home),
+              title: const Text('Home Page', style: TextStyle(fontSize: 18)),
               onTap: () {
                 // Here you can give your route to navigate
                 Navigator.pushReplacement(context,
                     MaterialPageRoute(builder: (context) => Homepage()));
               },
             ),
-            Divider(height: 3.0),
+            const Divider(height: 3.0),
             ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings', style: TextStyle(fontSize: 18)),
+              leading: const Icon(Icons.shop),
+              title: const Text('Shop', style: TextStyle(fontSize: 18)),
               onTap: () {
                 // Here you can give your route to navigate
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => Shop()));
               },
             ),
+            Divider(height: 3.0),
+          
             ListTile(
               leading: Icon(Icons.logout),
               title: Text('Log out', style: TextStyle(fontSize: 18)),
@@ -147,6 +147,28 @@ class _HomepageState extends State<Homepage> {
         actions: [
           Row(
             children: [
+              Container(
+                height: 40,
+                width: 30,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(blurRadius: 7, spreadRadius: 3, color: primarycolor)
+                ], shape: BoxShape.circle, color: primarycolor),
+                
+                child:
+                IconButton(
+            onPressed: () {
+             showSearch(context: context, delegate: Search());
+            },
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+          ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
              
               Container(
                 height: 40,
@@ -221,7 +243,7 @@ class _HomepageState extends State<Homepage> {
                     
                        
                           Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => ctgorysho(categoryname: cName)));
+                          MaterialPageRoute(builder: (context) => ctgshop(categoryname: cName)));
                     
                       },
                       categoryName: streamSnapshort.data!.docs[index]
@@ -241,7 +263,7 @@ class _HomepageState extends State<Homepage> {
              
                 const Padding(
                   padding: EdgeInsets.only(left: 20, top: 10),
-                  child: Text('Popular',
+                  child: Text('Popular products',
                       style:
                           TextStyle(fontWeight: FontWeight.w700, fontSize: 22)),
                 ),
@@ -278,35 +300,31 @@ class _HomepageState extends State<Homepage> {
                                     crossAxisAlignment:
                                         CrossAxisAlignment.start,
                                     children: [
-                                      Row(
-                                        children: [
-                                          Icon(
-                                            Icons.star,
-                                            color: primarycolor,
-                                            size: 20,
-                                          ),
-                                          SizedBox(width: 10),
-                                          Text(
-                                            'top of the week',
-                                            style: TextStyle(fontSize: 16),
-                                          )
-                                        ],
-                                      ),
-                                      SizedBox(height: 15),
-                                      SizedBox(
-                                        width:
-                                            MediaQuery.of(context).size.width /
-                                                2.2,
-                                        child: Text(
-                                          _products[index].pName,
-                                          style: TextStyle(fontSize: 16),
+                                        Row(
+                                          children: [
+                                           
+                                            
+                                            Text(
+                                              _products[index].pName,
+                                              style: const TextStyle(fontSize: 16),
+                                            )
+                                          ],
                                         ),
-                                      ),
-                                      Text(
-                                        _products[index].pCategory,
-                                        style: TextStyle(
-                                            fontSize: 16, color: Colors.grey),
-                                      ),
+                                       const  SizedBox(height: 15),
+                                        SizedBox(
+                                          width:
+                                              MediaQuery.of(context).size.width /
+                                                  2.2,
+                                          child: Text(
+                                            _products[index].pCategory,
+                                            style: const TextStyle(fontSize: 16),
+                                          ),
+                                        ),
+                                        Text(
+                                          "${_products[index].pPrice} \$",
+                                          style:const TextStyle(
+                                              fontSize: 16, color: Colors.grey),
+                                        ),
                                     ],
                                   ),
                                 ),
@@ -371,53 +389,9 @@ class _HomepageState extends State<Homepage> {
   
 
   
-  // Widget medCategoryCard(String? imagePath, String? name, int index) {
-  //   return GestureDetector(
-  //     onTap:() {
-        
-  //     },
-  //     child: Container(
-  //               margin: EdgeInsets.only(right: 20, top: 20, bottom: 20),
-  //               padding: EdgeInsets.symmetric(vertical: 25, horizontal: 20),
-  //               decoration: BoxDecoration(
-  //                   borderRadius: BorderRadius.circular(20),
-  //                   color: primarycolor,
-  //                   boxShadow: [
-  //                     BoxShadow(
-  //                       color: Colors.grey,
-  //                       blurRadius: 15,
-  //                     )
-  //                   ]),
-  //               child: Column(
-  //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //                 children: [
-  //                   Image.network(
-  //                      imagePath!,
-  //                       width: 40),
-                    // Text(name!,
-  //                       style: TextStyle(
-  //                         fontWeight: FontWeight.w800,
-  //                         fontSize: 16,
-  //                         color: Colors.white,
-  //                       )),
-  //                   RawMaterialButton(
-  //                       onPressed: null,
-  //                       fillColor: selectedFoodCard == index
-  //                           ? Colors.greenAccent
-  //                           : Colors.white,
-  //                       shape: CircleBorder(),
-  //                       child: Icon(Icons.chevron_right_rounded,
-  //                           size: 20,
-  //                           color: selectedFoodCard == index
-  //                               ? Colors.white
-  //                               : primarycolor))
-  //                 ],
-  //               ),
-  //             ),
-  //   );
             
           
-  // }
+
 }
 
 class Categories extends StatelessWidget {
@@ -465,4 +439,143 @@ class Categories extends StatelessWidget {
               ),
     );
   }
+}
+
+
+class Search extends SearchDelegate {
+
+  CollectionReference collectionReference = FirebaseFirestore.instance.collection("products");
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return [
+      IconButton(
+        icon: const Icon(Icons.close),
+        onPressed: () {
+          query = "";
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+  return StreamBuilder<QuerySnapshot>(
+        stream: (query != "")
+            ? FirebaseFirestore.instance.collection("products").snapshots()
+             : null,
+        builder: (context, AsyncSnapshot snapshot) {
+             if (snapshot.hasError) {
+               return const Text('Something went wrong');
+             }
+
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return Column(
+        children: const [
+          Center(
+              child: CupertinoActivityIndicator()
+          )
+        ],
+      );
+    }
+
+   
+
+    List<Product> products = snapshot.data.docs.map<Product>((doc) => Product(
+      
+     pCategory: doc["pCategory"], pDescription: doc["pDescription"],  pimageUrl: doc["pImage"], pName: doc["pName"], pPrice: doc["pPrice"], pID: doc.id,)).toList();
+    products = products.where((s) => s.pName.toLowerCase().contains(query.toLowerCase())).toList();
+    if(products.isEmpty){
+      return Column(
+        children: [
+         const  SizedBox(height: 100),
+          Center(
+            child: Text("No products available", style: TextStyle(fontSize: 20, color: primarycolor)),
+          )
+        ],
+      );
+
+    }
+    
+    
+    return
+        Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: (){
+                         Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    MedDetail(product: products[index])));
+                      },
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.white70, width: 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                        color: primarycolor,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: <Widget>[
+                                   
+                                  Image.network(
+                                 products[index].pimageUrl,
+                                    width: 150,
+                                    height: 100,
+                                    fit: BoxFit.fill,
+                                  ),
+                                  const SizedBox(
+                                    width: 25,
+                                  ),
+                                  Text(
+                                    products[index].pName,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                      ),
+                    );
+                  }
+              ),
+            ),
+          ],
+        );
+  },
+
+      );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+   return  Center(child:  Text("Search anything here", style: TextStyle(
+    color: primarycolor,
+    fontSize: 22,
+   ),));
+  }
+
 }
